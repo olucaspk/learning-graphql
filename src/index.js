54ad54be00735070ffca8b8a12d0ae8ea5816e84
Lucas <neexdeveloper@gmail.com>
2022-05-07 16:31:46 -0300

    :sparkles: getUserByEmail

--- src/index.js
+++ src/index.js
@@ -5,15 +5,43 @@ import { ApolloServer, gql } from 'apollo-server';
 // Mutation -> Alterar informações (POST/PUT/PATCH/DELETE)
 // Scalar Types -> String, Int, Float, Boolean, ID
 
+
+// A exclamação determina que é obrigatório o retorno
 const typeDefs = gql`
+    type User {
+        _id: ID!,
+        name: String!,
+        email: String!,
+        is_active: Boolean!
+    }
+
+    type Post {
+        _id: ID!,
+        title: String!,
+        content: String!,
+        author: User!
+    }
+    
     type Query {
         hello: String
+        users: [User!]! # ! -> declara que pode retornar [] mas não pode retornar null
+        getUserByEmail(email: String!): User!
     }
 `;
 
+const users = [
+    { _id: String(Math.random()), name: 'Lucas', email: 'lucas@smashv.gg', is_active: true },
+    { _id: String(Math.random()), name: 'Pedro', email: 'pedro@smashv.gg', is_active: false },
+    { _id: String(Math.random()), name: 'Nathalia', email: 'nathalia@smashv.gg', is_active: true },
+];
+
 const resolvers = {
     Query: {
-        hello: () => 'Hello World'
+        hello: () => 'Hello World',
+        users: () => users,
+        getUserByEmail: (_, args) => {
+            return users.find((user) => user.email === args.email);
+        }
     }
 }
 
